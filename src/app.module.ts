3062955cdb23517f517f1db383f19102ba23400d
Nabeelahh <saninabeelah@gmail.com>
2026-05-03 21:22:53 +0100

    project scafolding

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { ProofsModule } from './modules/proofs/proofs.module';
+import { StellarModule } from './modules/stellar/stellar.module';
+import { HealthModule } from './modules/health/health.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    HealthModule,
+    AuthModule,
+    UsersModule,
+    ProofsModule,
+    StellarModule,
+  ],
+})
+export class AppModule {}
