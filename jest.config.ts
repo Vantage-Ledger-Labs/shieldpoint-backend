c88b621b1c95c9e23ccad10e3efadb9da907c313
Abdulrahman Firdausi Onize <138733058+nanaabdul1172@users.noreply.github.com>
2026-05-16 10:08:18 +0000

    Futurenet/Testnet

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/*.spec.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  roots: ['<rootDir>/src'],
+};
+
+export default config;
